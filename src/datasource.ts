749e64866a2ba62d7cb6a9f95c5ba13ac226fde0
Jay (Work) <jaywork@Jensys-MacBook-Pro.local>
2024-10-03 08:53:56 -0400

    changes

--- src/datasource.ts
+++ src/datasource.ts
@@ -73,7 +73,7 @@ class CustomIntegration implements IntegrationBase {
   }
 
   async showVars(){
-    return [this.couch_db_url, this.couch_db_creds]
+    return [this.couch_db_url, this.couch_db_creds ?? 'NO CREDS']
   }
 
   async updateTokensInDb(curr_token_set:TokenSetParameters){
