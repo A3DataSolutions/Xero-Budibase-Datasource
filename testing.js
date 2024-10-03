3e149a405724f5423c30c6ce30ac12976de862c6
Jay (Work) <jaywork@Jensys-MacBook-Pro.local>
2024-10-03 09:15:54 -0400

    changes

---
+++ testing.js
@@ -0,0 +1,7 @@
+
+
+import nano from 'nano';
+
+
+const curr_nano = nano({url:'http://w6&wfu6a*KgY&@RpHnJFN!kakyAJT!kYuZoEM!NT:oJMr*G47oDX@Upqi^CuQz^XFVeAiV2t*9LNAM&oW@couchdb-service:5984'});
+console.log(curr_nano)
